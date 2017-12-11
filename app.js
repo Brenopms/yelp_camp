420bd8d678a0473d82a98ca41da5c9e377b05428
Breno Silva <brenosilva1@gmail.com>
2017-12-11 17:28:43 -0200

    add mon mongoose and connect to the app

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 'use strict';
-const bodyParser = require('body-parser');
-const express = require("express");
-const app = express();
+const bodyParser = require('body-parser'),
+ 	  express = require("express"),
+	  app = express(),
+      mongoose = require('mongoose');
+
+//create database
+mongoose.connect('mongodb://localhost/yelp_camp');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -9,19 +13,44 @@ app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
 const port = app.get('port');
 
-let campgrounds = [
-		{name: 'Salmon Creek' , image: 'https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg'},
-		{name: 'Granite Hill', image:'https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg'},
-		{name: 'Mountain Goats Rest', image: 'https://farm5.staticflickr.com/4100/4814089244_feda3fd1fb.jpg'},
 
-];
+//Schema setup
+
+let campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+
+});
+
+let Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+// 	{name: 'Salmon Creek' , image: 'https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg'},
+// 	(error, campground) => {
+// 		if (error){
+// 			console.log(error);
+// 		}
+// 		else {
+// 			console.log('Newly created campground');
+// 			console.log(campground);
+// 		}
+// 	});
+
 
 app.get('/' , (req, res, next) => {
 	res.render("landing");
 });
 
 app.get('/campgrounds', (req, res, next) => {
-	res.render('campgrounds', {campgrounds: campgrounds});
+	// res.render('campgrounds', {campgrounds: campgrounds});
+	Campground.find({}, (error, allCampgrounds) => {
+		if(error){
+			console.log(error);
+		} else {
+			res.render('campgrounds', {campgrounds:allCampgrounds})
+		}
+
+	});
 });
 
 app.get('/campgrounds/new', (req, res, next) => {
@@ -33,9 +62,16 @@ app.post('/campgrounds', (req, res, next) => {
 	let name = req.body.name;
 	let image = req.body.image;
 	let newCampground = {name:name , image: image};
-	campgrounds.push(newCampground);
-	//redirect back to the campground route
-	res.redirect('/campgrounds');
+	//create a new campground and save it to the DB
+	Campground.create(newCampground, (error, newlyCreated) => {
+		if(error){
+			console.log(error);
+		} else {
+			//redirect back to the campground route
+			res.redirect('/campgrounds');
+		}
+	});
+
 })
 
 app.listen(port, () => {
